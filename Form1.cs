bd33fdd8afd720208e4e9198978c8589d0970098
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-19 00:49:26 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseDoubleClick += Form1_MouseDoubleClick; 
+        }
+
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = colorDialog.Color;
+            }
+        }
+    }
+}
